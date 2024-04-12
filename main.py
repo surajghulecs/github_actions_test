5c75914ff9896615bd90ae6904c17082f94b27a4
Suraj Baliram Ghule <33917478+surajghulecs@users.noreply.github.com>
2024-04-12 13:30:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("hello world")
 
 
 def welcome():
-  print("welcome to github actions testing")
+    print("welcome to github actions testing")
 
 
 print("This is a tutorial for github actions")
